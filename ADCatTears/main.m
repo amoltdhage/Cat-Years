940407df1bf9372b046b3d4299b1ad8cf2a73ee9
Felix Training P_02 <felixtrainingp_02@Felix-ITs-Pimpri-02.local>
2016-07-29 14:01:41 +0530

    Initial Commit

---
+++ ADCatTears/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ADCatTears
+//
+//  Created by Felix Training P_02 on 29/07/16.
+//  Copyright © 2016 Amol Dhage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
